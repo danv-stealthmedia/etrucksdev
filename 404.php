21b7899b56d878bd6b3ebf5f2b07a25b042490ed
dan <dan@stealthmedia.com>
2020-11-03 10:29:26 -0600

    404 added

--- 404.php
+++ 404.php
@@ -1,89 +1,46 @@
 <?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @package understrap
- */
 
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+	get_header();
+
+	$size = 'page-header';
 
 get_header();
-
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
+<?php get_template_part("nav"); ?>
 
-<div class="wrapper" id="error-404-wrapper">
+	<div class="wrapper" id="single-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
+			<main class="site-main pagetext" id="main">
 
-			<div class="col-md-12 content-area" id="primary">
+					<article class="single-page" id="post-<?php the_ID(); ?>">
+					<div class="custom-padding">
+						<header class="entry-header">
+							<div class="post-title text-primary"><h1 class="text-blue">Error 404</h1></div>
 
-				<main class="site-main" id="main">
 
-					<section class="error-404 not-found">
+						</header>
+						<div class="entry-content">
+							<p>The page you are looking for was not found</p>
+						</div><!-- .entry-content -->
+					</div>
+					<div class="background-primary free-trial-section">
+						<div class="row paddingleftright pt-4 pb-4  align-items-center justify-content-center flex-column"><p class="text-white">We want to make IFTA reports and other fuel tax reports easy for you.
+								Why not take advantage of our 31-day free trial?</p>
+							<a class="btn btn-secondary" href="https://app.etrucks.com/signup">START YOUR FREE 31 DAY TRIAL</a>
+						</div>
+					</div>
+					</article><!-- #post-## -->
 
-						<header class="page-header">
-
-							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'understrap' ); ?></h1>
-
-						</header><!-- .page-header -->
-
-						<div class="page-content">
-
-							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'understrap' ); ?></p>
-
-							<?php get_search_form(); ?>
-
-							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-							<?php if ( understrap_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-
-								<div class="widget widget_categories">
-
-									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'understrap' ); ?></h2>
-
-									<ul>
-										<?php
-										wp_list_categories(
-											array(
-												'orderby'    => 'count',
-												'order'      => 'DESC',
-												'show_count' => 1,
-												'title_li'   => '',
-												'number'     => 10,
-											)
-										);
-										?>
-									</ul>
-
-								</div><!-- .widget -->
-
-							<?php endif; ?>
-
-							<?php
-
-							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'understrap' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-							the_widget( 'WP_Widget_Tag_Cloud' );
-							?>
-
-						</div><!-- .page-content -->
-
-					</section><!-- .error-404 -->
-
-				</main><!-- #main -->
-
-			</div><!-- #primary -->
+			</main><!-- #main -->
 
 		</div><!-- .row -->
 
 	</div><!-- #content -->
 
-</div><!-- #error-404-wrapper -->
+</div><!-- #single-wrapper -->
 
 <?php get_footer();
